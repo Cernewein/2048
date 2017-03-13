c79c2d194fe251de1b0cea9d7471847f44cbbe84
fgouttes <flo.gsol@hotmail.fr>
2017-03-13 17:34:43 +0100

    Add files via upload
    Ajout des fonction haut et bas de DamierDyn

--- damierdyn.h
+++ damierdyn.h
@@ -1,11 +1,28 @@
 #ifndef DAMIERDYN_H
 #define DAMIERDYN_H
 
+#include <iostream>
+using namespace std;
 
 class DamierDyn
 {
 public:
-    DamierDyn();
+    DamierDyn(int vd=0);
+    DamierDyn(const DamierDyn &D);
+    ~DamierDyn();
+
+    void Print();
+    void Init(int value);
+    void Set(int x, int y, int value);
+
+private:
+    int L;
+    int C;
+    int** T;
+
+    // Méthode privée (factorisation  de code)
+    void Alloc();
+    void Free();
 };
 
 #endif // DAMIERDYN_H
