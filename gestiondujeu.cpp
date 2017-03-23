e652e81a2cb70f2c4665b9a848c6ac12e0eda899
fgouttes <flo.gsol@hotmail.fr>
2017-03-23 11:13:48 +0100

    Add files via upload
    Correction de bugs et amélioration des couleurs : première version fonctionnelle :)

--- gestiondujeu.cpp
+++ gestiondujeu.cpp
@@ -36,16 +36,40 @@ QList<QString> GestionDuJeu::readCouleur()
                 l.append(QString::fromStdString("#c3bcbc"));
             }
             else if(number == 2){
-                l.append(QString::fromStdString("yellow"));
+                l.append(QString::fromStdString("#ffffff"));
             }
             else if(number == 4){
-                l.append(QString::fromStdString("orange"));
+                l.append(QString::fromStdString("#fffee4"));
             }
             else if(number == 8){
-                l.append(QString::fromStdString("red"));
+                l.append(QString::fromStdString("#ffd58b"));
+            }
+            else if(number == 16){
+                l.append(QString::fromStdString("#ffc781"));
+            }
+            else if(number == 32){
+                l.append(QString::fromStdString("#ffba6b"));
+            }
+            else if(number == 64){
+                l.append(QString::fromStdString("#ffa95d"));
+            }
+            else if(number == 128){
+                l.append(QString::fromStdString("#f9ff79"));
+            }
+            else if(number == 256){
+                l.append(QString::fromStdString("#ffee2e"));
+            }
+            else if(number == 512){
+                l.append(QString::fromStdString("#ff8e1d"));
+            }
+            else if(number == 1024){
+                l.append(QString::fromStdString("#ff7563"));
+            }
+            else if(number == 2048){
+                l.append(QString::fromStdString("#ff2020"));
             }
             else{
-                l.append(QString::fromStdString("#c3bcbc"));
+                l.append(QString::fromStdString("#de1b1b"));
             }
 
         }
@@ -56,7 +80,12 @@ QList<QString> GestionDuJeu::readCouleur()
 void GestionDuJeu::toucheHaut(){
     if (grille.HautPossible()){
         grille.Haut();
+        cout<<"Touche Haut";
+        cout<<endl;
+        grille.Print();
+        cout<<endl;
         geneAlea();
+        grille.Print();
     }
     cptChanged();
 }
@@ -64,7 +93,12 @@ void GestionDuJeu::toucheHaut(){
 void GestionDuJeu::toucheBas(){
     if (grille.BasPossible()){
         grille.Bas();
+        cout<<"Touche Bas";
+        cout<<endl;
+        grille.Print();
+        cout<<endl;
         geneAlea();
+        grille.Print();
     }
     cptChanged();
 }
@@ -72,7 +106,12 @@ void GestionDuJeu::toucheBas(){
 void GestionDuJeu::toucheDroite(){
     if (grille.DroitePossible()){
         grille.Droite();
+        cout<<"Touche Droite";
+        cout<<endl;
+        grille.Print();
+        cout<<endl;
         geneAlea();
+        grille.Print();
     }
     cptChanged();
 }
@@ -80,13 +119,18 @@ void GestionDuJeu::toucheDroite(){
 void GestionDuJeu::toucheGauche(){
     if (grille.GauchePossible()){
         grille.Gauche();
+        cout<<"Touche Gauche";
+        cout<<endl;
+        grille.Print();
+        cout<<endl;
         geneAlea();
+        grille.Print();
     }
     cptChanged();
 }
 
 void GestionDuJeu::geneAlea(){
-    cout<<"I'm here";
+    //cout<<"I'm here";
     DamierDyn libres(0,0);
     int nb_libres = 0;
     for(int i=0;i<4;i++){ //On cherche dans la grille de jeu, une fois un mouvement effectué sur quelles cases on peut générer des nouveau nombres
@@ -100,10 +144,10 @@ void GestionDuJeu::geneAlea(){
         }
     }
     libres.Print();
-    int alea = rand() % (nb_libres-1);
-    //cout<<alea<<endl;
-    int x = libres.ret(alea-1,0);
-    int y = libres.ret(alea-1,1);
+    int alea = rand() % (nb_libres);
+    cout<<alea<<endl;
+    int x = libres.ret(alea,0);
+    int y = libres.ret(alea,1);
     grille.Set(x,y,2);
     cptChanged();
 }
